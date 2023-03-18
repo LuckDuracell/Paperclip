d991516d020f2caf1b79346b9f23ea00e0e62cca
LuckDev <lukedru@yahoo.com>
2023-03-17 19:06:03 -0700

    Initial Commit

---
+++ Paperclip/PaperclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperclipApp.swift
+//  Paperclip
+//
+//  Created by Luke Drushell on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PaperclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
